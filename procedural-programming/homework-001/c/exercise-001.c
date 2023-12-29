1a89b0029ddb68cb4072c27cea341c16798801dd
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:03:10 -0300

    feat: read and print an integer number

---
+++ procedural-programming/homework-001/c/exercise-001.c
@@ -0,0 +1,19 @@
+/* Student: Pedro Henrique Resende Ribeiro
+ * Description: Read and print an integer number
+ * Date: 2023-12-28 - Time: 02:00
+ */
+
+#include <stdio.h>
+
+int main() {
+    
+    int number;
+
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+
+    printf("The number is %d\n", number);
+
+    return 0;
+
+}
