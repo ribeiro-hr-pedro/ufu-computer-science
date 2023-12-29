854ce762594b82f1293a80f20f827571f22cbfb4
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:07:58 -0300

    feat: read and print a real number

---
+++ procedural-programming/homework-001/python/exercise_002.py
@@ -0,0 +1,6 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Read and print a real number
+# Date: 2023-12-28 - Time: 02:10
+
+number = float(input('Enter a real number: '))
+print(f'The number is {number}')
