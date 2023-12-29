b19c5aa2d630bdcc4748d1ea1bdfb108a4213b0d
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:08:10 -0300

    feat: sum of three numbers

---
+++ procedural-programming/homework-001/python/exercise_003.py
@@ -0,0 +1,11 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Sum of three numbers
+# Date: 2023-12-28 - Time: 02:20
+
+number_1 = float(input('Enter the 1st number: '))
+number_2 = float(input('Enter the 2nd number: '))
+number_3 = float(input('Enter the 3rd number: '))
+
+sum = number_1 + number_2 + number_3
+
+print(f'The sum is {sum}')
