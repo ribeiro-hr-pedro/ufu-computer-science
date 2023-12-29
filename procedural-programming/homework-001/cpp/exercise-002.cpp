713738d04b2c0853419513fb36ac5e869e0c7ab1
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:06:26 -0300

    feat: read and print a real number

---
+++ procedural-programming/homework-001/cpp/exercise-002.cpp
@@ -0,0 +1,21 @@
+/* Student: Pedro Henrique Resende Ribeiro
+ * Description: Read and print a real number
+ * Date: 2023-12-28 - Time: 02:10
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double number;
+
+    cout << "Enter a real number: ";
+    cin >> number;
+
+    cout << "The number is " << number << endl;
+
+    return 0;
+
+}
