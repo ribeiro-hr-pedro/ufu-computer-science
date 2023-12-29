0efba6714a564e4a81920482da127bd45b4d2092
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:07:06 -0300

    feat: print the fifth part of a number

---
+++ procedural-programming/homework-001/cpp/exercise-005.cpp
@@ -0,0 +1,23 @@
+/* Student: Pedro Henrique Resende Ribeiro
+ * Description: Print the fifth part of a number
+ * Date: 2023-12-29 - Time: 00:10
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double number, answer;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    answer = number / 5.0;
+
+    cout << "The fifth part is " << answer << endl;
+
+    return 0;
+
+}
