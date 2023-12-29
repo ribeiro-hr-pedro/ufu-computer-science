abd2668f7ca85ee9e6ac00e91e08c4f24608ac72
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:05:43 -0300

    feat: read and print an integer number

---
+++ procedural-programming/homework-001/cpp/exercise-001.cpp
@@ -0,0 +1,21 @@
+/* Student: Pedro Henrique Resende Ribeiro
+ * Description: Read and print an integer number
+ * Date: 2023-12-28 - Time: 02:00
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int number;
+
+    cout << "Enter an integer number: ";
+    cin >> number;
+
+    cout << "The number is " << number << endl;
+
+    return 0;
+
+}
