75bb9a1490945c992f3b44dadd4f77589a2fd45f
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:06:53 -0300

    feat: print the square of a number

---
+++ procedural-programming/homework-001/cpp/exercise-004.cpp
@@ -0,0 +1,23 @@
+/* Student: Pedro Henrique Resende Ribeiro
+ * Description: Print the square of a number
+ * Date: 2023-12-29 - Time: 00:00
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double number, answer;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    answer = number * number;
+
+    cout << "The number squared is " << answer << endl;
+
+    return 0;
+
+}
