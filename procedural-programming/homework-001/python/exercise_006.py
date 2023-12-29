4ca0f1922ab0e775e1158d064554bfe7e6d55752
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:08:48 -0300

    feat: convert from celsius to fahrenheit

---
+++ procedural-programming/homework-001/python/exercise_006.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from celsius to fahrenheit
+# Date: 2023-12-29 - Time: 00:20
+
+celsius = float(input('Enter a temperature in celsius: '))
+
+fahrenheit = celsius * (9.0 / 5.0) + 32.0
+
+print(f'The temperature in fahrenheit is {fahrenheit}')
