bb5a19d9f7acf2a37693060e71c0273cfec3b1f5
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:07:45 -0300

    feat: read and print an integer number

---
+++ procedural-programming/homework-001/python/exercise_001.py
@@ -0,0 +1,6 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Read and print an integer number
+# Date: 2023-12-28 - Time: 02:00
+
+number = int(input('Enter an integer number: '))
+print(f'The number is {number}')
