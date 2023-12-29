61691150b102910df6e1f9d061925589628855bd
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:03:35 -0300

    feat: read and print a real number

---
+++ procedural-programming/homework-001/c/exercise-002.c
@@ -0,0 +1,19 @@
+/* Student: Pedro Henrique Resende Ribeiro
+ * Description: Read and print a real number
+ * Date: 2023-12-28 - Time: 02:10
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    float number;
+
+    printf("Enter a real number: ");
+    scanf("%f", &number);
+
+    printf("The number is %f\n", number);
+
+    return 0;
+
+}
