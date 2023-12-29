06d975c77e2c3070078c20f51ec443f7ae09ddb3
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:08:23 -0300

    feat: print the square of a number

---
+++ procedural-programming/homework-001/python/exercise_004.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Print the square of a number
+# Date: 2023-12-29 - Time: 00:00
+
+number = float(input('Enter a number: '))
+
+answer = number ** 2.0
+
+print(f'The number squared is {answer}')
