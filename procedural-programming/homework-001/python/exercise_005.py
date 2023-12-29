47bc3963d4c194092934da9922a8974cb4aeefb7
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-29 01:08:37 -0300

    feat: print the fifth part of a number

---
+++ procedural-programming/homework-001/python/exercise_005.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Print the fifth part of a number
+# Date: 2023-12-29 - Time: 00:00
+
+number = float(input('Enter a number: '))
+
+answer = number / 5.0
+
+print(f'The fifth part is {answer}')
