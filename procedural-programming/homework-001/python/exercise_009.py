d1615deb743d14f1b958c4cb1d9e8c03949d37f0
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-30 17:06:03 -0300

    feat: convert from celsius to kelvin

---
+++ procedural-programming/homework-001/python/exercise_009.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from celsius to kelvin
+# Date: 2023-12-30 - Time: 16:50
+
+celsius = float(input('Enter a temperature in celsius: '))
+
+kelvin = celsius + 273.15
+
+print(f'The temperature in kelvin is {kelvin}')
