b2610343be0826a923cf8e86d1295efbd8574809
Pedro <ribeiro.hr.pedro@gmail.com>
2023-12-31 20:33:41 -0300

    feat: convert from m/s to km/h

---
+++ procedural-programming/homework-001/python/exercise_011.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from m/s to km/h
+# Date: 2023-12-31 - Time: 20:10
+
+ms = float(input('Enter a speed in m/s: '))
+
+kmh = ms * 3.6
+
+print(f'The speed in km/h is {kmh}')
