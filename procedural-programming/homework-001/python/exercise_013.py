6461f27565ee1545af579f173632614fa967c613
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-01 16:14:15 -0300

    feat: convert from kilometers to miles

---
+++ procedural-programming/homework-001/python/exercise_013.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from kilometers to miles
+# Date: 2024-01-01 - Time: 15:00
+
+kilometers = float(input('Enter a distance in kilometers: '))
+
+miles = kilometers / 1.61
+
+print(f'The distance in miles is {miles}')
