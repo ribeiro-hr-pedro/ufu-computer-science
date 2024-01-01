47728d40630d641e8deab98e31b3d01a520c6b6e
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-01 16:14:24 -0300

    feat: convert from degrees to radians

---
+++ procedural-programming/homework-001/python/exercise_014.py
@@ -0,0 +1,11 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from degrees to radians
+# Date: 2024-01-01 - Time: 15:10
+
+PI = 3.14
+
+degrees = float(input('Enter an angle in degrees: '))
+
+radians = degrees * PI / 180.0
+
+print(f'The angle in radians is {radians}')
