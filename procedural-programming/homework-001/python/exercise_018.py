19273c7577b1a020e27d7fd637a2f3b9fd235bba
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-04 23:41:18 -0300

    feat: convert from m3 to liters

---
+++ procedural-programming/homework-001/python/exercise_018.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from m3 to liters
+# Date: 2024-01-04 - Time: 22:20
+
+m3 = float(input('Enter a volume in cubic meters: '))
+
+liters = m3 * 1000.0
+
+print(f'The volume in liters is {liters}')
