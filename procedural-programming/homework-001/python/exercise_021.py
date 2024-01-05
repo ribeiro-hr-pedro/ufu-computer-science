c653ee4d9333cfb5b7c30dab6731b87e679368b3
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-04 23:41:57 -0300

    feat: convert from pounds to kilograms

---
+++ procedural-programming/homework-001/python/exercise_021.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from pounds to kilograms
+# Date: 2024-01-04 - Time: 22:50
+
+pounds = float(input('Enter a mass in pounds: '))
+
+kilograms = pounds * 0.45
+
+print(f'The mass in kilograms is {kilograms}')
