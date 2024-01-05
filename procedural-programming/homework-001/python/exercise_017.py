f3d2c2b9b34cf1d92ad39d559d9a8e5925598288
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-04 23:41:00 -0300

    feat: convert from centimeters to inches

---
+++ procedural-programming/homework-001/python/exercise_017.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from centimeters to inches
+# Date: 2024-01-04 - Time: 22:10
+
+centimeters = float(input('Enter a length in centimeters: '))
+
+inches = centimeters / 2.54
+
+print(f'The length in inches is {inches}')
