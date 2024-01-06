69b5b1b0290f6d3f68c3d6d739b5a1590b587253
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-06 16:36:44 -0300

    feat: sum of squares of 3 numbers

---
+++ procedural-programming/homework-001/python/exercise_028.py
@@ -0,0 +1,11 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Sum of squares of 3 numbers
+# Date: 2024-01-06 - Time: 15:00
+
+number_1 = float(input('Enter the 1st number: '))
+number_2 = float(input('Enter the 2nd number: '))
+number_3 = float(input('Enter the 3rd number: '))
+
+sum = number_1 ** 2.0 + number_2 ** 2.0 + number_3 ** 2.0
+
+print(f'The sum of squares is {sum}')
