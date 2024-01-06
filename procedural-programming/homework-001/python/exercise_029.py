c254ca2e0da33abecb5c352c19faf593e14251b8
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-06 16:36:59 -0300

    feat: average of 4 grades

---
+++ procedural-programming/homework-001/python/exercise_029.py
@@ -0,0 +1,12 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Average of 4 grades
+# Date: 2024-01-06 - Time: 15:10
+
+grade_1 = float(input('Enter the 1st grade: '))
+grade_2 = float(input('Enter the 2nd grade: '))
+grade_3 = float(input('Enter the 3rd grade: '))
+grade_4 = float(input('Enter the 4th grade: '))
+
+average = (grade_1 + grade_2 + grade_3 + grade_4) / 4.0
+
+print(f'The average is {average}')
