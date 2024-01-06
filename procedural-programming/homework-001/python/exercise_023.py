8c10a6975b1dcfbb5059c53a2d90db86a93536cc
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-06 16:35:24 -0300

    feat: convert from meters to yards

---
+++ procedural-programming/homework-001/python/exercise_023.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from meters to yards
+# Date: 2024-01-06 - Time: 14:10
+
+meters = float(input('Enter a length in meters: '))
+
+yards = meters / 0.91
+
+print(f'The length in yards is {yards}')
