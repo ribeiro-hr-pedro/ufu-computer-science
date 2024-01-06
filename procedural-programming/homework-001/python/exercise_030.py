dd7f2d221e70ffc49a18951a0989a528a1337369
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-06 16:37:19 -0300

    feat: convert from real to dollar

---
+++ procedural-programming/homework-001/python/exercise_030.py
@@ -0,0 +1,10 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from real to dollar
+# Date: 2024-01-06 - Time: 15:20
+
+real = float(input('Enter a value in real: '))
+rate = float(input('Enter the exchange rate: '))
+
+dollar = real * rate
+
+print(f'The value in dollar is {dollar}')
