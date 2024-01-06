7693c3d0f3c0f75105a87229963e29a7a0e9870a
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-06 16:36:14 -0300

    feat: convert from m2 to hectares

---
+++ procedural-programming/homework-001/python/exercise_026.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from m2 to hectares
+# Date: 2024-01-06 - Time: 14:40
+
+m2 = float(input('Enter an area in square meters: '))
+
+hectares = m2 * 0.0001
+
+print(f'The area in hectares is {hectares}')
