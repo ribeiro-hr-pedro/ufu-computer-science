6a98a21718f2b265962b06d288eaed95d0601e8d
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-06 16:35:40 -0300

    feat: convert from m2 to acres

---
+++ procedural-programming/homework-001/python/exercise_024.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Convert from m2 to acres
+# Date: 2024-01-06 - Time: 14:20
+
+m2 = float(input('Enter an area in square meters: '))
+
+acres = m2 * 0.000247
+
+print(f'The area in acres is {acres}')
