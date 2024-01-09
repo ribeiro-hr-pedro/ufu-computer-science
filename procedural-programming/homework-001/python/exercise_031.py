3ce7e1672f6aa4865a3d4a05603f256baffc32fa
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-09 02:18:13 -0300

    feat: predecessor and successor of a number

---
+++ procedural-programming/homework-001/python/exercise_031.py
@@ -0,0 +1,8 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Predecessor and successor of a number
+# Date: 2024-01-09 - Time: 00:40
+
+number = int(input('Enter an integer number: '))
+
+print(f'Predecessor: {number - 1}')
+print(f'Successor: {number + 1}')
