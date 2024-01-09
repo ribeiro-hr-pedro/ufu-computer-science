f5d24996e16c072996a785beeca1e05be807ae12
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-09 02:18:38 -0300

    feat: print the area of a square

---
+++ procedural-programming/homework-001/python/exercise_033.py
@@ -0,0 +1,9 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Print the area of a square
+# Date: 2024-01-09 - Time: 01:00
+
+side = float(input('Enter the side size of the square: '))
+
+area = side ** 2.0
+
+print(f'The area is {area}')
