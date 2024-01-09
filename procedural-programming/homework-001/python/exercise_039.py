34330b0fee3ce5f3d44ce1670d8db76a5d46011c
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-09 02:20:52 -0300

    feat: print the amount won by each winner

---
+++ procedural-programming/homework-001/python/exercise_039.py
@@ -0,0 +1,13 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Print the amount won by each winner
+# Date: 2024-01-09 - Time: 02:00
+
+prize = 780000.0
+
+value1 = prize * 0.46
+value2 = prize * 0.32
+value3 = prize * 0.22
+
+print(f'Winner 1: {value1:,.2f}')
+print(f'Winner 2: {value2:,.2f}')
+print(f'Winner 3: {value3:,.2f}')
