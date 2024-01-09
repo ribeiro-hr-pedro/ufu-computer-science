db6f7b35c065d99fd1833a1886e517a0efc3003f
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-09 02:18:51 -0300

    feat: print the area of a circle

---
+++ procedural-programming/homework-001/python/exercise_034.py
@@ -0,0 +1,11 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Print the area of a circle
+# Date: 2024-01-09 - Time: 01:10
+
+PI = 3.141592
+
+radius = float(input('Enter the radius of the circle: '))
+
+area = PI * radius ** 2.0
+
+print(f'The area is {area}')
